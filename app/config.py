a8126d5e9010a4a1c12309dd3b19cfcb0f2069d5
sasanpy <sasan.sp92@hotmail.com>
2021-11-06 01:15:38 +0330

    complete phase 1 of project.

---
+++ app/config.py
@@ -0,0 +1,12 @@
+# Postgresql configs
+POST_HOST = 'smspostgresql'
+POST_HOST_USERNAME = 'postgres'
+POST_HOST_PASSWORD = 'test'
+POST_HOST_DB_NAME = 'smspostgresql'
+
+# Login credentials
+PASSWORD = 'pass'
+USERNAME = 'user'
+
+# Generate one strong secret key for flask.
+SECRET_KEY = 'top secret'
